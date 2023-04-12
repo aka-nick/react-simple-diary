546909808a7ff0ee60aecb250cee0133b7990377
aka-nick <bechalrang@gmail.com>
2023-04-12 16:28:58 +0900

    여러 state(author, content)를 하나의 객체로 관리해보기

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,5 +1,37 @@
+import { useState } from "react";
+
 const DiaryEditor = () => {
-  return <div className="DiaryEditor"></div>;
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+  });
+
+  const changeAuthor = (e) => {
+    console.log(e.target.value);
+    setState({ author: e.target.value, content: state.content });
+  };
+
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          value={state.author}
+          onChange={(e) => {
+            changeAuthor(e);
+          }}
+        />
+      </div>
+      <div>
+        <textarea
+          value={state.content}
+          onChange={(e) => {
+            setState({ author: state.author, content: e.target.value });
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default DiaryEditor;
